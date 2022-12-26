eaaae20a2be863ae4171890053c53c6f8a1454e7
gmrtnz <genis.martinez@biotechvana.com>
2022-12-26 12:44:23 +0100

    picrust foe server, TODO testing

---
+++ set_picrust.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+#Install conda first
+
+cd /root/
+wget https://github.com/picrust/picrust2/archive/v2.4.1.tar.gz
+tar xvzf  v2.4.1.tar.gz
+cd picrust2-2.4.1/
+
+conda env create -f  picrust2-env.yaml
+conda activate picrust2
+pip install --editable .
